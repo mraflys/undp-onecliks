e8060acbbe9db009704cf6e3675d2250f9357a8d
Rafly <rafly.syahputra@usenobi.com>
2025-12-03 15:21:26 +0700

    change query

--- app/RequestQuery.php
+++ app/RequestQuery.php
@@ -7,57 +7,53 @@ class RequestQuery
 {
     public static function ongoing($where = null)
     {
-        $where = is_null($where) ? "tr.date_deleted IS NULL AND tr.id_status IN (-1, 1, 2, 5, 6, 7)" : $where;
+        $where = is_null($where) ? "date_deleted IS NULL" : $where;
 
-        $query = "SELECT
-                         tr.id_transaction, tr.transaction_code, tr.agency_name_buyer, tr.id_user_buyer, tr.person_name_buyer,
-                         tr.service_name, tr.date_authorized, tr.id_agency_unit_service, tr.comment,
-                         su.agency_unit_name AS agency_name_service, tr.id_status, tr.date_transaction,
-                         tr.service_price, tr.description, st.status_name,
-                         tr.date_finished,
-                         IFNULL(wf.workflow_name, '-') AS workflow_name,
-                         IFNULL(wf.date_end_estimated, '-') AS date_end_estimated,
-                         IFNULL(wf.date_start_actual, '-') AS date_start_actual,
-                         IFNULL(wf.id_transaction_workflow, 0) AS id_transaction_workflow,
-                         IFNULL(wf.id_agency_unit_pic, '-') AS id_agency_unit_pic,
-                         a.id_agency_unit, a.agency_unit_name AS parent_agency_name, c.country_name,
-                         IFNULL(wf.delay, 0) AS delay,
-                         IFNULL(wf.id_user_pic_primary, 0) AS id_user_pic_primary,
-                         IFNULL(wf.id_user_pic_alternate, 0) AS id_user_pic_alternate,
-                         IFNULL(u1.person_name, '-') AS person_name_primary,
-                         IFNULL(u2.person_name, '-') AS person_name_alternate,
-                         IFNULL(wf.sequence, 0) AS sequence,
-                         IFNULL(tr.date_rating, '-') AS date_rating,
-                         p.id_project, p.project_code, p.project_name
-                  FROM tr_service tr
-                  INNER JOIN ms_agency_unit su ON tr.id_agency_unit_service = su.id_agency_unit AND su.date_deleted IS NULL
-                  INNER JOIN ms_agency_unit a ON su.id_agency_unit_parent = a.id_agency_unit AND a.date_deleted IS NULL
-                  INNER JOIN ms_country c ON a.id_country = c.id_country AND c.date_deleted IS NULL
-                  LEFT JOIN (
-                      SELECT
-                          g.id_transaction_parent,
-                          wf.workflow_name,
-                          wf.date_end_estimated,
-                          wf.date_start_actual,
-                          wf.id_transaction_workflow,
-                          wf.id_agency_unit_pic,
-                          wf.id_user_pic_primary,
-                          wf.id_user_pic_alternate,
-                          wf.sequence,
-                          fn_get_number_workday(wf.date_end_estimated, IFNULL(g.date_finished, NOW()), false) AS delay
-                      FROM tr_service_workflow wf
-                      INNER JOIN tr_service g ON g.id_transaction = wf.id_transaction
-                          AND g.transaction_code IS NULL
-                          AND g.date_deleted IS NULL
-                      WHERE wf.date_start_actual IS NOT NULL
-                          AND wf.date_end_actual IS NULL
-                          AND wf.date_deleted IS NULL
-                  ) wf ON wf.id_transaction_parent = tr.id_transaction
-                  LEFT JOIN ms_status st ON st.id_status = tr.id_status AND st.date_deleted IS NULL
-                  LEFT JOIN sec_user u1 ON u1.id_user = wf.id_user_pic_primary AND u1.date_deleted IS NULL
-                  LEFT JOIN sec_user u2 ON u2.id_user = wf.id_user_pic_alternate AND u2.date_deleted IS NULL
-                  LEFT JOIN ms_project p ON tr.id_project = p.id_project
-                  WHERE $where";
+        $query = "SELECT  tr.id_transaction, tr.transaction_code, tr.agency_name_buyer, tr.id_user_buyer, tr.person_name_buyer, tr.service_name, tr.date_authorized, tr.id_agency_unit_service, tr.comment, su.agency_unit_name AS agency_name_service, tr.id_status, tr.date_transaction, tr.service_price, tr.description, status_name,
+                    '-' AS date_finished, '-' AS workflow_name, '-' AS date_end_estimated, '-' AS date_start_actual, 0 AS id_transaction_workflow, '-' AS id_agency_unit_pic,
+                    a.id_agency_unit, a.agency_unit_name AS parent_agency_name, c.country_name, 0 AS delay, 0 AS id_user_pic_primary, 0 AS id_user_pic_alternate,
+                    '-' AS person_name_primary, '-' AS person_name_alternate, 0 AS sequence, '-' AS date_rating, p.id_project, p.project_code, p.project_name
+            FROM (SELECT * FROM tr_service WHERE date_deleted IS NULL AND id_status = -1 AND $where) tr
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) su ON tr.id_agency_unit_service = su.id_agency_unit
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) a ON su.id_agency_unit_parent=a.id_agency_unit
+            JOIN (SELECT id_country, country_name FROM ms_country WHERE date_deleted IS NULL) c ON a.id_country=c.id_country
+            JOIN ms_status st ON st.id_status=tr.id_status
+            LEFT JOIN (SELECT id_project, project_code, project_name FROM ms_project) p ON tr.id_project=p.id_project
+            UNION
+            SELECT  tr.id_transaction, tr.transaction_code, tr.agency_name_buyer, tr.id_user_buyer, tr.person_name_buyer, tr.service_name, tr.date_authorized,
+                    tr.id_agency_unit_service, tr.comment, su.agency_unit_name AS agency_name_service, tr.id_status, tr.date_transaction, tr.service_price, tr.description, status_name, tr.date_finished, '-' AS workflow_name, '-' AS date_end_estimated, '-' AS date_start_actual, 0 AS id_transaction_workflow, '-' AS id_agency_unit_pic, a.id_agency_unit, a.agency_unit_name AS parent_agency_name, c.country_name, fn_get_number_workday( date_end_estimated, tr.date_finished, false) AS is_delay, 0 AS id_user_pic_primary, 0 AS id_user_pic_alternate, '-' AS person_name_primary, '-' AS person_name_alternate, 0 AS sequence,
+                    tr.date_rating, p.id_project, p.project_code, p.project_name
+            FROM (SELECT * FROM tr_service WHERE date_deleted IS NULL AND id_status IN (5, 6, 7) AND date_rating IS NULL AND $where) tr
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) su ON tr.id_agency_unit_service = su.id_agency_unit
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) a ON su.id_agency_unit_parent=a.id_agency_unit
+            JOIN (SELECT id_country, country_name FROM ms_country WHERE date_deleted IS NULL) c ON a.id_country=c.id_country
+            LEFT JOIN (SELECT * FROM ms_status WHERE date_deleted IS NULL) st ON st.id_status=tr.id_status
+            JOIN (
+                  SELECT g.id_transaction_parent, MAX(date_end_estimated) AS date_end_estimated
+                  FROM (SELECT * FROM tr_service_workflow WHERE date_deleted IS NULL) wf
+                  JOIN (SELECT * FROM tr_service WHERE transaction_code IS NULL AND date_deleted IS NULL) g ON g.id_transaction=wf.id_transaction
+                  GROUP BY g.id_transaction_parent
+                  ) g ON g.id_transaction_parent=tr.id_transaction
+            LEFT JOIN (SELECT id_project, project_code, project_name FROM ms_project) p ON tr.id_project=p.id_project
+            UNION
+            SELECT  tr.id_transaction, tr.transaction_code, tr.agency_name_buyer, tr.id_user_buyer, tr.person_name_buyer, tr.service_name, tr.date_authorized,
+                    tr.id_agency_unit_service, tr.comment, su.agency_unit_name AS agency_name_service, tr.id_status, tr.date_transaction, tr.service_price, tr.description, status_name, '-' AS date_finished, wf.workflow_name, date_end_estimated, date_start_actual, id_transaction_workflow, wf.id_agency_unit_pic,
+                    a.id_agency_unit, a.agency_unit_name AS parent_agency_name, c.country_name, fn_get_number_workday(date_end_estimated, now(), false) AS delay,
+                    wf.id_user_pic_primary, wf.id_user_pic_alternate, COALESCE(u1.person_name, '-') AS person_name_primary, COALESCE(u2.person_name, '-')  AS person_name_alternate, wf.sequence, '-' AS date_rating, p.id_project, p.project_code, p.project_name
+            FROM (SELECT * FROM tr_service WHERE date_deleted IS NULL AND id_status IN (1, 2) AND $where) tr
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) su ON tr.id_agency_unit_service = su.id_agency_unit
+            JOIN (SELECT * FROM ms_agency_unit WHERE date_deleted IS NULL) a ON su.id_agency_unit_parent=a.id_agency_unit
+            JOIN (SELECT id_country, country_name FROM ms_country WHERE date_deleted IS NULL) c ON a.id_country=c.id_country
+            JOIN (
+                  SELECT id_transaction_parent, workflow_name, date_end_estimated, date_start_actual, id_transaction_workflow, id_agency_unit_pic,
+                  id_user_pic_primary, id_user_pic_alternate, wf.sequence
+                  FROM (SELECT * FROM tr_service_workflow WHERE date_start_actual IS NOT NULL AND date_end_actual IS NULL AND date_deleted IS NULL) wf
+                  JOIN (SELECT * FROM tr_service WHERE transaction_code IS NULL AND date_deleted IS NULL) g ON g.id_transaction=wf.id_transaction
+                  ) wf ON wf.id_transaction_parent=tr.id_transaction
+            JOIN ms_status st ON st.id_status=tr.id_status
+            LEFT JOIN (SELECT * FROM sec_user WHERE date_deleted IS NULL) u1 ON u1.id_user=wf.id_user_pic_primary
+            LEFT JOIN (SELECT * FROM sec_user WHERE date_deleted IS NULL) u2 ON u2.id_user=wf.id_user_pic_alternate
+            LEFT JOIN (SELECT id_project, project_code, project_name FROM ms_project) p ON tr.id_project=p.id_project";
 
         return DB::select($query);
     }
